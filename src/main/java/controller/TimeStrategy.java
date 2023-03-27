a822d4ba3ce02df1486bddd4511824925846693f
Simina <baciu.gr.maria@student.utcluj.ro>
2023-03-27 14:12:56 +0300

    Queues management application

--- src/main/java/controller/TimeStrategy.java
+++ src/main/java/controller/TimeStrategy.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class TimeStrategy implements controller.Strategy {
+public class TimeStrategy implements Strategy {
     @Override
     public void addTask(List<Server> servers, Task t) {
         servers.get(servers.size() - 1).addTask(t);
